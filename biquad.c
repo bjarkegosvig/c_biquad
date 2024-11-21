8c2f1e09cb4f83f4b4dd7b70a3ce5122238f38ec
Bjarke Knudsen (BJKN) <bjkn@bang-olufsen.dk>
2024-11-21 15:34:59 +0100

    First skeleton of a cascade implemention

--- biquad.c
+++ biquad.c
@@ -11,7 +11,7 @@ int bq_df1_init(struct bq_struct* bq_inst, const bq_type coefs[5])
     for (size_t i = 0; i < 3; i++) {
         bq_inst->coefs[i] = coefs[i];
     }
-    // invert coefficients on the a coefficients, so the output can be written as
+    // invert the sign on the a coefficients, so the output can be written as
     // y[i] = b0*x[i] + b1*x[i-1] + b2*x[i-2] + a1*y[i-1] + a2*y[i-2]
     bq_inst->coefs[3] = -coefs[3];
     bq_inst->coefs[4] = -coefs[4];
@@ -21,22 +21,50 @@ int bq_df1_init(struct bq_struct* bq_inst, const bq_type coefs[5])
 
 void bq_df1_process(struct bq_struct* const bq_inst, const bq_type* __restrict input, bq_type* __restrict output, size_t input_len)
 {
-
-    int8_t w1, w2, w3;
-
+    // init the delay line indexes
+    int8_t w1 = bq_inst->w0 ^ 1; // toggle w1 between 0 and 1. it must be 1 when w0 is 0 and visa versa
+    int8_t w2 = bq_inst->w0 + 2;
+    int8_t w3 = w1 + 2;
     for (size_t i = 0; i < input_len; i++) {
-        // init the delay line indexes
-        w1 = bq_inst->w0 ^ 1; // toggle w1 between 0 and 1. it must be 1 when w0 is 0 and visa versa
-        w2 = bq_inst->w0 + 2;
-        w3 = w1 + 2;
-
         // calculate biquad
         output[i] = (input[i] * bq_inst->coefs[0]) + (bq_inst->delay_line[bq_inst->w0] * bq_inst->coefs[1]) + (bq_inst->delay_line[w1] * bq_inst->coefs[2]) + (bq_inst->delay_line[w2] * bq_inst->coefs[3]) + (bq_inst->delay_line[w3] * bq_inst->coefs[4]);
 
         // update delay line
         bq_inst->w0 = bq_inst->w0 ^ 1; // toggle w0 between 0 and 1
         w2 = bq_inst->w0 + 2;
+        w3 = w1 + 2;
         bq_inst->delay_line[bq_inst->w0] = input[i];
         bq_inst->delay_line[w2] = output[i];
     }
+}
+
+
+void bq_cascade_df1_init(struct bq_cas_struct* bq_inst, size_t num_of_biquads, bq_type* coefs, bq_type* delay_line)
+{
+    int32_t idx = 3;
+
+    bq_inst->coefs = coefs;
+    for (size_t i = 0; i < num_of_biquads; i++)
+    {
+
+        // invert the sign on the a coefficients, so the output can be written as
+        // y[i] = b0*x[i] + b1*x[i-1] + b2*x[i-2] + a1*y[i-1] + a2*y[i-2]
+        bq_inst->coefs[idx] = -bq_inst->coefs[idx]; 
+        idx++;
+        bq_inst->coefs[idx] = -bq_inst->coefs[idx];
+        idx += 3;
+    }
+
+    bq_inst->delay_line = delay_line;
+    for (size_t i = 0; i < num_of_biquads*4; i++)
+    {
+        bq_inst->delay_line[i] = 0;
+    }
+    
+    bq_inst->stages = num_of_biquads;
+    bq_inst->w0 = 0;
+}
+
+void bq_cascade_df1_process(struct bq_cas_struct* const bq_inst, const bq_type* __restrict input, bq_type* __restrict output, size_t input_len)
+{
 }
--- biquad.h
+++ biquad.h
@@ -43,4 +43,38 @@ void bq_df1_process(struct bq_struct* const bq_inst, const bq_type* __restrict i
 
 
 
+/**
+ * @struct Instance structure for a cascade set of biquad filter
+ */
+struct bq_cas_struct {
+    bq_type* coefs;  /**< points to the coefficient array. */ 
+    bq_type* delay_line; /**< points to the delay line array. */
+    int8_t w0; /**< The delay line index pointing to x[i-1] */
+    int8_t stages; /**< Number of biquads */
+};
+
+/**
+* Initialize the direct form I biquad structure.
+* @param bq_inst : cascade biquad structure, which holds the coefficients and delay elements.
+* @param num_of_biquads : number of biquads in the cascade filter
+* @param coef: a pointer to array of biquad coefficients on the form {b00, b01, b02, a01, a02, b10, b11, b12, a11, a12, b20, ...}. 
+*
+* @note The coefficients must be of the form where it satisfies the form
+ * y[i] = b0*x[i] + b1*x[i-1] + b2*x[i-2] - a1*y[i-1] - a2*y[i-2]
+*/
+
+void bq_cascade_df1_init(struct bq_cas_struct* bq_inst, size_t num_of_biquads, bq_type* coefs, bq_type* delay_line);
+
+/**
+* Process a direct form I biquad.
+* @param *bq_inst : biquad structure, which holds the coefficients and delay elements.
+* @param *input : input data array.
+* @param *output : pointer to output array
+* @param input_len : size/length of the input array
+
+* @note The biquad will process using the diract form I equation seen below.
+* y[i] = b0*x[i] + b1*x[i-1] + b2*x[i-2] - a1*y[i-1] - a2*y[i-2]
+*/
+
+void bq_cascade_df1_process(struct bq_cas_struct* const bq_inst, const bq_type* __restrict input, bq_type* __restrict output, size_t input_len);
 #endif
